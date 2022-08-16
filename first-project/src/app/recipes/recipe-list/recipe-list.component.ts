e16514b05ec67fc4d29f593a508b4e0ed3494f23
wpj0110 <84047863+wpj0110@users.noreply.github.com>
2022-08-15 21:34:32 -0500

    Buttons and Stuff Added. Missing Functionalities

---
+++ first-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model'
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Cheescake', 'A Cheesecake is a cake comprised of cheese','https://assets.bonappetit.com/photos/5c48972f52d8785eddd4a72c/master/pass/burnt-basque-cheesecake-1.jpg')
+    , new Recipe('Applie Pie', 'A Pie with Apple','https://www.inspiredtaste.net/wp-content/uploads/2019/11/Homemade-Apple-Pie-From-Scratch-1200.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ first-project/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Ingredient } from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient("Milk", 100),
+    new Ingredient("Cheese", 400)
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
